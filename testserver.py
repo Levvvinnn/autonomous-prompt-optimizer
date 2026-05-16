325a7ef67abbcaa25d2b1ed01d4a623c9e16e21b
Levin <levinmathewshibu@gmail.com>
2026-05-16 12:34:17 +0400

    test server

---
+++ testserver.py
@@ -0,0 +1,9 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'working')
+
+HTTPServer(('127.0.0.1', 9000), Handler).serve_forever()
